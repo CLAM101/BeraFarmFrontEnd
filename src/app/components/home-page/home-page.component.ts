67948ce1b50cf20746d938985884976cf6529c7e
Darren Bullock <darren.bullock.gppos.net>
2024-02-14 09:36:10 +0200

    wording and styling changes

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+  styleUrl: './home-page.component.css',
 })
 export class HomePageComponent {
+  constructor() {}
 
+  infoPage = false;
+
+  ngOnInit(): void {}
+
+  showInfo() {
+    this.infoPage = true;
+  }
 }

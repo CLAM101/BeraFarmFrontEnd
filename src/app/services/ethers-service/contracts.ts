86e4e6e0387e417cc2c4070fa695bb7c8ecce6ed
Darren Bullock <darren.bullock@gppos.net>
2024-12-11 08:49:26 +0200

    update contracts

--- src/app/services/ethers-service/contracts.ts
+++ src/app/services/ethers-service/contracts.ts
@@ -1,5 +1,5 @@
-export const beraFarm = '0x9A9f2CCfdE556A7E9Ff0848998Aa4a0CFD8863AE';
-export const mockHoney = '0xA51c1fc2f0D1a1b8494Ed1FE312d7C3a78Ed91C0';
-export const fuzzToken = '0x9A676e781A523b5d0C0e43731313A708CB607508';
-export const beraCub = '0xB7f8BC63BbcaD18155201308C8f3540b07f84F5e';
-export const marketPlace = '0x0B306BF915C4d645ff596e518fAf3F9669b97016';
+export const beraFarm = '0x11a795b8F8cDEf1636B9b925A32D4AC2D405FAC8';
+export const mockHoney = '0x14FFb27fe7376af2f0C1ac1C74658C18d2305D4d';
+export const fuzzToken = '0xdb502f6eb12944F36777259064154DE687D956a5';
+export const beraCub = '0xa70e4586d7bEF00adEA7c258512231E067D3eEa5';
+export const marketPlace = '0x104890ffDe265DC7c8f201D31D8373C7a7FF57AF';
